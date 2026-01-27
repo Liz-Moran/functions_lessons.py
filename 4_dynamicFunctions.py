030cfe8cc2b7aebd60cedb47ccf5b92460b468c5
Liz-Moran <lizmor0812@gmail.com>
2026-01-27 21:13:08 +0000

    Functions

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -2,15 +2,19 @@
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
 
 # Don't call the function, you just need to define it.
+values = [1,2,3,4]
+def all_positives(input_list):
+    for number in input_list:
+        if number <=0:
+            return False
+    else:
+        return True
 
-
-
-
-
-
+print(all_positives())
 # Dynamic Functions Practice #2
 # Create a function (sum_less) that adds the numbers of a list as long as they are greater than 0 and less than 1000, and returns the result of said sum. Create a numbers variable, storing a list of numbers so we can test it.
 
+def sum_less(list):
 
 
 
